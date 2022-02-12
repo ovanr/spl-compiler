6aa530bf80982b62215dca5fda78e61150b6fe18
joshdev-de <joshua.steinmann@ru.nl>
2022-02-12 19:03:11 +0100

    remove faulted import

--- test/SPL/Compiler/Lexer/Test.hs
+++ test/SPL/Compiler/Lexer/Test.hs
@@ -3,11 +3,15 @@
 {-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
 {-# HLINT ignore "Use camelCase" #-}
 
+#define ALEX_MONAD_BYTESTRING
+
 module SPL.Compiler.Lexer.Test (htf_thisModulesTests) where
 
+
 import Test.Framework
-import SPL.Compiler.Lexer.AlexLexGen (tokenize, Token(..), Keyword(..))
+import SPL.Compiler.Lexer.AlexLexGen (tokenize, Token(..), Keyword(..), SPLToken(..), Type(..))
 import qualified Data.ByteString.Lazy as B
+
 
 tokenize_file fl = do
     contents <- B.readFile fl
@@ -19,11 +23,11 @@ test_tokenize_at_sign_in_identifier = do
 
 test_tokenize_multi_line_comment = do
     actual <- tokenize_file "test/SPL/Compiler/Lexer/multi_line_comment.spl"
-    let expected = [KeywordToken While, 
-                    IdentifierToken "id23", 
-                    IdentifierToken "how", 
-                    IntToken 233, 
-                    IdentifierToken "okay",
+    let expected = [MkToken (AlexPn 0 1 1) (KeywordToken While),
+                    MkToken (AlexPn 8 2 2) (IdentifierToken "id23"),
+                    MkToken (AlexPn 27 4 2) (IdentifierToken "how"),
+                    MkToken (AlexPn 69 9 2) (IntToken 233),
+                    MkToken (AlexPn 73 9 6) (IdentifierToken "okay"),
                     EOF]
     
     assertEqual actual (Right expected)
