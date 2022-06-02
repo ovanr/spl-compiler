fa22dac1f4ae955a652cc75a7ba1073aedac7d28
joshdev-de <joshua.steinmann@ru.nl>
2022-06-02 19:07:27 +0200

    minor fix

--- src/SPL/Compiler/.CodeGen/Backend/SSMGen.hs
+++ src/SPL/Compiler/.CodeGen/Backend/SSMGen.hs
@@ -268,6 +268,86 @@ handleFunDef (IR.IRFunDef (IR.IRFunDecl' (IR.IRFunDecl "_snd_assign" _ _)) _) = 
     SSM.str RR
     removeStackFrame
 
+handleFunDef (IR.IRFunDef (IR.IRFunDecl' (IR.IRFunDecl "store_thunk" _ _)) _) = do
+    newBlock "store_thunk"
+    SSM.link 1
+    SSM.ldl (-2)
+    SSM.ldc 4
+    SSM.add
+    SSM.stl 1
+    newBlock "store_thunk_loop"
+    SSM.ldl 1
+    SSM.ldc 1
+    SSM.eq
+    SSM.brt "end_store_thunk"
+    SSM.ldr MP
+    SSM.ldl 1
+    SSM.sub
+    SSM.lda 0
+    SSM.sth
+    SSM.ldl 1
+    SSM.ldc 1
+    SSM.sub
+    SSM.stl 1
+    SSM.bra "store_thunk_loop"
+    newBlock "end_store_thunk"
+    SSM.str RR
+    removeStackFrame
+
+handleFunDef (IR.IRFunDef (IR.IRFunDecl' (IR.IRFunDecl "call_thunk" _ _)) _) = do
+    newBlock "call_thunk"
+    SSM.link 1
+    SSM.ldl (-3)
+    SSM.stl 1
+    newBlock "load_args_loop"
+    SSM.ldl 1
+    SSM.ldc 1
+    SSM.ge
+    SSM.brf "load_thunk"
+    SSM.ldr MP 
+    SSM.ldl 1
+    SSM.sub 
+    SSM.lda (-3)
+    SSM.ldl 1
+    SSM.ldc 1
+    SSM.sub 
+    SSM.stl 1
+    SSM.bra "load_args_loop"
+    newBlock "load_thunk"
+    SSM.ldl (-2)
+    SSM.lda 0
+    SSM.ldc 3
+    SSM.add 
+    SSM.stl 1
+    newBlock "load_thunk_loop"
+    SSM.ldl 1
+    SSM.ldc 1
+    SSm.ge 
+    SSM.brf "eval_thunk"
+    SSM.ldl (-2)
+    SSM.ldl 1
+    SSM.sub 
+    SSM.lda 1
+    SSM.ldl 1
+    SSM.ldc 1
+    SSM.sub 
+    SSM.stl 1
+    SSM.bra "load_thunk_loop"
+    newBlock "eval_thunk"
+    SSM.ldl (-3)
+    SSM.add 
+    SSM.lds (-1)
+    SSM.lds (-1)
+    SSM.eq 
+    SSM.brt "eval_saturated_thunk"
+    SSM.bsr "store_thunk"
+    removeStackFrame
+    newBlock "eval_saturated_thunk"
+    SSM.stl 1
+    SSM.stl 1
+    SSM.jsr
+    removeStackFrame
+
 handleFunDef def@(IR.IRFunDef (IR.IRFunDecl' decl@(IR.IRFunDecl label args _)) body) = do
     newBlock label
     let argVars = extractArgsVars decl
